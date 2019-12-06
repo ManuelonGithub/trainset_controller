7f139762dcc62c2f8b6e8312ae288e7c6053e2db
Manuel <manuel.burnay@andra.com>
2019-12-06 17:32:41 -0400

    expanded definitions and started work on track manager

---
+++ src/trainset/track_manager.h
@@ -0,0 +1,9 @@
+
+
+#ifndef TRACK_SERVER_H
+#define TRACK_SERVER_H
+
+
+
+
+#endif // TRACK_SERVER_H
--- src/trainset/trainset_defs.h
+++ src/trainset/trainset_defs.h
@@ -3,6 +3,8 @@
 #ifndef TRAINSET_DEFINITIONS_H
 #define TRAINSET_DEFINITIONS_H
 
+#include <stdint.h>
+
 #define PACKET_BOX          14
 #define TRACK_BOX           13
 
@@ -10,5 +12,21 @@
 #define TRACK_NAME  "Track Manager"
 #define TRAIN_NAME  "Train Manager"
 
+typedef enum TRAIN_DIRECTIONS {CW, CCW} direction_t;
+
+typedef struct trainset_msg_ {
+    uint8_t code;
+    uint8_t arg1;
+    uint8_t arg2;
+} trainset_msg_t;
+
+typedef union train_ctrl_ {
+    uint8_t arg;
+    struct {
+        uint8_t     mag : 4;
+        uint8_t     res : 3;
+        direction_t dir : 1;
+    };
+} train_ctrl_t;
 
 #endif // TRAINSET_DEFINITIONS_H
