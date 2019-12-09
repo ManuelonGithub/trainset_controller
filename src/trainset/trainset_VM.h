62a78a3d93a7d856ab0812c5c82326a37ddf57c3
Callum <callum.cottrell@mac.com>
2019-12-09 15:36:52 -0400

    forgot this header file

--- src/trainset/trainset_VM.h
+++ src/trainset/trainset_VM.h
@@ -1,3 +1,19 @@
 
 
+#ifndef TRAINSET_VM_H
+#define TRAINSET_VM_H
+
+struct CUPch
+{
+char esc;
+char sqrbrkt;
+char line[2];   /* 01 through 24 */
+char semicolon;
+char col[2];    /* 01 through 80 */
+char cmdchar;
+char nul;
+char ch;
+};
 void userIO(void);
+
+#endif //
